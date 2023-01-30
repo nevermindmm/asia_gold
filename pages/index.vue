12c2830e65571b2cd220d3f0f9f716d2509bb39e
folkspch <folk_spch@hotmail.com>
2023-01-30 20:53:54 +0700

    add doughnut chart

--- pages/index.vue
+++ pages/index.vue
@@ -1,69 +1,29 @@
 <template>
   <div>
-    <v-card max-width="50%" class="pa-5">
-      <Bar
-        :chart-options="chartOptions"
-        :chart-data="chartData"
-        :chart-id="chartId"
-        :dataset-id-key="datasetIdKey"
-        :plugins="plugins"
-        :css-classes="cssClasses"
-        :styles="styles"
-        :width="width"
-        :height="height"
-      />
-    </v-card>
+    <v-row>
+      <v-col>
+        <v-card max-width="100%" class="pa-5">
+          <Bar :chart-options="chartOptions" :chart-data="chartData" />
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card max-width="100%" class="pa-5">
+          <Doughnut :chart-options="chartOptions" :chart-data="chartData" />
+        </v-card>
+      </v-col>
+    </v-row>
   </div>
 </template>
   
   <script>
-import { Bar } from 'vue-chartjs'
-import {
-  Chart as ChartJS,
-  Title,
-  Tooltip,
-  Legend,
-  BarElement,
-  CategoryScale,
-  LinearScale,
-} from 'chart.js'
-
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+import { Bar, Doughnut } from 'vue-chartjs'
+import Chart from 'chart.js/auto'
 
 export default {
   // middleware: 'auth',
   name: 'BarChart',
-  components: { Bar },
-  props: {
-    chartId: {
-      type: String,
-      default: 'bar-chart',
-    },
-    datasetIdKey: {
-      type: String,
-      default: 'label',
-    },
-    width: {
-      type: Number,
-      default: 0,
-    },
-    height: {
-      type: Number,
-      default: 0,
-    },
-    cssClasses: {
-      default: '',
-      type: String,
-    },
-    styles: {
-      type: Object,
-      default: () => {},
-    },
-    plugins: {
-      type: Object,
-      default: () => {},
-    },
-  },
+  components: { Bar, Doughnut },
+  props: {},
   data() {
     return {
       chartData: {
