45050218bac7f2ed9ed95ae080ea71bf27b55d5a
folkspch <folk_spch@hotmail.com>
2023-04-06 05:16:04 +0700

    report almost

--- pages/report.vue
+++ pages/report.vue
@@ -2,8 +2,7 @@
   <div>
     <v-card class="pa-3 mb-6" elevation="1">
       <v-card-title
-        ><v-icon large>mdi-text-box</v-icon
-        >&nbsp;|&nbsp;รายงาน</v-card-title
+        ><v-icon large>mdi-text-box</v-icon>&nbsp;|&nbsp;รายงาน</v-card-title
       >
       <v-breadcrumbs :items="items" class="pa-0 ml-5">
         <template v-slot:divider>
@@ -15,10 +14,30 @@
     <v-card>
       <v-row justify="center" class="pa-3">
         <v-col cols="2">
-          <v-select dense :items="example" label="เดือน" outlined></v-select>
+          <v-select
+            @change="getSalesData()"
+            v-model="selected_month"
+            color="red"
+            item-color="red"
+            item-text="thMonth"
+            item-value="intMonth"
+            dense
+            :items="month"
+            label="เดือน"
+            outlined
+          ></v-select>
         </v-col>
         <v-col cols="2">
-          <v-select dense :items="example" label="ปี" outlined></v-select>
+          <v-select
+            @change="getSalesData()"
+            v-model="selected_year"
+            color="red"
+            item-color="red"
+            dense
+            :items="year"
+            label="ปี"
+            outlined
+          ></v-select>
         </v-col>
         <v-col>
           <v-btn outlined color="success">
@@ -40,6 +59,7 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   data: () => ({
     items: [
@@ -54,63 +74,65 @@ export default {
         href: 'breadcrumbs_link_1',
       },
     ],
-    example: ['choice1', 'choice2', 'choice3', 'choice4', 'choice5', 'choice6'],
+    month: null,
+    year: null,
+    selected_month: null,
+    selected_year: null,
     headers: [
-      {
-        text: 'วันที่',
-        align: 'start',
-        value: 'date',
-      },
-      { text: 'Shopee', value: 'shopee' },
-      { text: 'Lazada', value: 'lazada' },
-      { text: 'Line Shop', value: 'lineshop' },
-      { text: 'JD Central', value: 'jd' },
-      { text: 'ยอดรวม', value: 'sum' },
+      // {
+      //   text: 'วันที่',
+      //   align: 'start',
+      //   value: 'sale_day',
+      // },
+      // { text: 'Shopee', value: 'platform_1' },
+      // { text: 'Lazada', value: 'platform_2' },
+      // { text: 'Line Shop', value: 'platform_3' },
+      // { text: 'JD Central', value: 'jd' },
+      // { text: 'ยอดรวม', value: 'total_sales' },
     ],
-    data: [
-      {
-        date: 1,
-        shopee: 200000,
-        lazada: 50000,
-        lineshop: 30000,
-        jd: 0,
-        sum: 280000,
-      },
-      {
-        date: 2,
-        shopee: 200000,
-        lazada: 20000,
-        lineshop: 30000,
-        jd: 0,
-        sum: 250000,
-      },
-      {
-        date: 3,
-        shopee: 20000,
-        lazada: 100000,
-        lineshop: 80000,
-        jd: 50000,
-        sum: 250000,
-      },
-      {
-        date: 4,
-        shopee: 200000,
-        lazada: 50000,
-        lineshop: 0,
-        jd: 30000,
-        sum: 280000,
-      },
-      {
-        date: 5,
-        shopee: 30000,
-        lazada: 200000,
-        lineshop: 50000,
-        jd: 0,
-        sum: 280000,
-      },
-      
-    ],
+    data: [],
   }),
+  methods: {
+    getSelect() {
+      axios.get('http://localhost:4000/getLimitOfSales').then((res) => {
+        this.month = res.data.month.slice()
+        this.year = res.data.year.slice()
+      })
+    },
+    getSalesData() {
+      let body = {
+        month: this.selected_month,
+        year: this.selected_year,
+      }
+      if (body.month && body.year) {
+        axios.post('http://localhost:4000/getSalesData', body).then((res) => {
+          this.data = res.data.data.slice()
+          let platform = res.data.platform.slice()
+          let header = [
+            {
+              text: 'วันที่',
+              align: 'start',
+              value: 'sale_day',
+            }
+          ]
+          for(let i=0;i<platform.length;i++){
+            header.push({text:`${platform[i].platform_name}`,
+            align:'start',
+            value: `platform_${platform[i].platform_id}`
+            })
+          }
+          header.push({ text: 'ยอดรวม',align:'start', value: 'total_sales' })
+          this.headers = header.slice()
+        })
+      }
+    },
+    setHeader() {
+      let header = []
+    },
+  },
+  mounted() {
+    this.getSelect()
+  },
 }
 </script>
 
