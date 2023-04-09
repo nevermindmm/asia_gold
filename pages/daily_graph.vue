99e9393a508057a2d4216cae5066329b919eae8b
folkspch <folk_spch@hotmail.com>
2023-04-09 13:04:12 +0700

    daily_graph

--- pages/daily_graph.vue
+++ pages/daily_graph.vue
@@ -1,13 +1,138 @@
 <template>
-  
+  <div>
+    <v-card class="pa-3 mb-6" elevation="1">
+      <v-card-title
+        ><v-icon large>mdi-account-edit</v-icon
+        >&nbsp;|&nbsp;กราฟยอดขายรายวัน</v-card-title
+      >
+      <v-breadcrumbs :items="items" class="pa-0 ml-5">
+        <template v-slot:divider>
+          <v-icon>mdi-chevron-right</v-icon>
+        </template>
+      </v-breadcrumbs>
+    </v-card>
+    <v-row justify="center">
+      <v-card width="50%" height="60%" class="pa-5">
+      <v-select
+        @change="getSales()"
+        placeholder="แพลตฟอร์ม"
+        v-model="selected_platform"
+        style="width: 40%"
+        dense
+        outlined
+        item-color="red"
+        color="red"
+        :items="plaform_list"
+        item-text="platform_name"
+        item-value="platform_id"
+      ></v-select>
+      <div v-if="this.chartData.datasets[0].data.length > 0">
+        <v-row justify="center" class="pb-3">
+          <v-card max-width="60%" elevation="0">
+            <Doughnut :chart-options="chartOptions" :chart-data="chartData" />
+          </v-card>
+        </v-row>
+      </div>
+      <div v-else>
+        <v-row justify="center" class="pb-3">
+          <v-card-text class="d-flex justify-center">ไม่มีข้อมูล</v-card-text>
+        </v-row>
+      </div>
+    </v-card>
+    </v-row>
+    
+  </div>
 </template>
 
 <script>
+import { Bar, Doughnut } from 'vue-chartjs'
+import Chart from 'chart.js/auto'
+import axios from 'axios'
 export default {
+  components: { Bar, Doughnut },
+  data: () => ({
+    selected_platform: null,
+    plaform_list: [],
+    items: [
+      {
+        text: 'หน้าหลัก',
+        disabled: false,
+        href: '/',
+      },
+      {
+        text: 'กราฟยอดขายรายวัน',
+        disabled: true,
+        href: 'breadcrumbs_link_1',
+      },
+    ],
+    chartData: {
+      labels: [],
+      datasets: [
+        {
+          data: [],
+          backgroundColor: [
+            'rgb(238, 77, 45)',
+            'rgb(69, 108, 204)',
+            'rgb(6, 199, 85)',
+          ],
+        },
+      ],
+    },
+    chartOptions: {
+      responsive: true,
+    },
+  }),
+  methods: {
+    getSales() {
+      let today = new Date()
+      let month = today.getMonth() + 1
+      let year = today.getFullYear()
+      let date = today.getDate()
+      let body = {
+        month: month,
+        year: year,
+        date: date,
+        platform: this.selected_platform,
+      }
+      this.chartData.labels = []
+      this.chartData.datasets[0].data = []
+      axios.post('http://localhost:4000/graphData', body).then((res) => {
+        // let platform = res.data.sales
+        // for (let i = 0; i < platform.length; i++) {
+        //   this.chartData.datasets[0].data.push(platform[i].total_sales)
+        //   this.chartData.labels.push(platform[i].platform_name)
+        // }
+        let best_seller = res.data.best_seller
+        if (best_seller) {
+          for (let i = 0; i < best_seller.length; i++) {
+            this.chartData.labels.push(best_seller[i].type)
+            this.chartData.datasets[0].data.push(best_seller[i].qty)
+          }
+        } else {
+          this.chartData.labels = []
+          this.chartData.datasets[0].data = []
+        }
 
+        //  = label.slice()
+        // this.chartData2.labels = label.slice()
+      })
+    },
+    getPlatformList() {
+      axios.get('http://localhost:4000/getPlatformList').then((res) => {
+        for (let i = 0; i < res.data.data.length; i++) {
+          this.plaform_list.push({
+            platform_id: res.data.data[i].platform_id,
+            platform_name: res.data.data[i].platform_name,
+          })
+        }
+      })
+    },
+  },
+  mounted() {
+    this.getPlatformList()
+  },
 }
 </script>
 
 <style>
-
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -11,18 +11,17 @@
       </v-col>
       <v-col cols="6">
         <v-card max-width="100%" class="pa-5">
-          <Doughnut :chart-options="chartOptions" :chart-data="chartData" />
+          <Doughnut :chart-options="options1" :chart-data="chartData" />
         </v-card>
       </v-col>
     </v-row>
-    
   </div>
 </template>
   
   <script>
 import { Bar, Doughnut } from 'vue-chartjs'
 import Chart from 'chart.js/auto'
-
+import axios from 'axios'
 export default {
   // middleware: 'auth',
   name: 'BarChart',
@@ -31,36 +30,78 @@ export default {
   data() {
     return {
       chartData: {
-        labels: ['Shopee', 'Lazada', 'Line Shop', 'JD Central'],
+        labels: [],
         datasets: [
           {
-            label: 'ยอดขายต่อเดือนในแต่ละแพลดฟอร์ม',
-            data: [40000, 20000, 50000, 40000],
+            label: 'ยอดขายต่อเดือนในแต่ละแพลตฟอร์ม(บาท)',
+            data: [],
             backgroundColor: [
               'rgb(238, 77, 45)',
               'rgb(69, 108, 204)',
-              'rgb(6, 199, 85)', 
-              'black',
+              'rgb(6, 199, 85)',
             ],
           },
         ],
       },
       chartData2: {
-        labels: ['สร้อยข้อมือ', 'สร้อยคอ', 'แหวน'],
+        labels: [],
         datasets: [
           {
-            label: 'สินค้าขายดี',
-            data: [40000, 20000, 50000],
-            backgroundColor: [
-              'rgb(255,215,0)',
-            ],
+            label: 'สินค้าขายดี(ชิ้น)',
+            data: [],
+            backgroundColor: ['rgb(255,215,0)'],
           },
         ],
       },
       chartOptions: {
         responsive: true,
       },
+      options1: {
+        tooltips: {
+          callbacks: {
+            label: function (tooltipItem, data) {
+              // Get the data for the current tooltip item
+              const dataset = data.datasets[tooltipItem.datasetIndex]
+              const value = dataset.data[tooltipItem.index]
+
+              // Calculate the percentage and format as a string
+              const total = dataset.data.reduce((acc, cur) => acc + cur, 0)
+              const percent = ((value / total) * 100).toFixed(2) + '%'
+
+              // Return the formatted label
+              return `${data.labels[tooltipItem.index]}: ${percent}`
+            },
+          },
+        },
+      },
     }
+  },
+  methods: {
+    getSales() {
+      let today = new Date()
+      let month = today.getMonth() + 1
+      let year = today.getFullYear()
+      let body = { month: month, year: year }
+      let label = []
+      axios.post('http://localhost:4000/graphData', body).then((res) => {
+        console.log(res)
+        let platform = res.data.sales
+        for (let i = 0; i < platform.length; i++) {
+          this.chartData.datasets[0].data.push(platform[i].total_sales)
+          this.chartData.labels.push(platform[i].platform_name)
+        }
+        let best_seller = res.data.best_seller
+        for (let i = 0; i < best_seller.length; i++) {
+          this.chartData2.labels.push(best_seller[i].type)
+          this.chartData2.datasets[0].data.push(best_seller[i].qty)
+        }
+        //  = label.slice()
+        // this.chartData2.labels = label.slice()
+      })
+    },
+  },
+  mounted() {
+    this.getSales()
   },
 }
 </script>
